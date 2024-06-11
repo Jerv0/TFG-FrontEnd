ccbbc2c77796edf4dc8cd571e292186869d16b40
Jerv0 <ruizmartinezjavier99@gmail.com>
2024-06-11 14:29:40 +0200

    Subo proyecto android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tfgFront.app',
+  appName: 'tfg-proyecto',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src-capacitor/android/app/src/main/java/com/tfgFront/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tfgFront.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
